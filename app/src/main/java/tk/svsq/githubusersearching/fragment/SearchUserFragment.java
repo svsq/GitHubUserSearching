c4ea06d4ac53762124440da028c061e750bc9e8b
Slaventij Bilokur <svsq@outlook.com>
2018-04-14 13:55:37 +0300

    Now search function is working properly (but shows only one result).

--- app/src/main/java/tk/svsq/githubusersearching/fragment/SearchUserFragment.java
+++ app/src/main/java/tk/svsq/githubusersearching/fragment/SearchUserFragment.java
@@ -9,24 +9,35 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import tk.svsq.githubusersearching.R;
+import tk.svsq.githubusersearching.model.GitHubUser;
+import tk.svsq.githubusersearching.rest.GitHubApiClient;
+import tk.svsq.githubusersearching.rest.GitHubUserCall;
 
 public class SearchUserFragment extends Fragment implements View.OnClickListener {
 
     public static final String KEY_COMPANY_NAME = "username";
     public static final String KEY_NUMBER_REPO = "number_repo";
 
-    private RelativeLayout layout;
+    private FrameLayout layout;
     private ImageView userAvatar;
     private TextView userNameText;
     private TextView userLocationText;
@@ -44,7 +55,7 @@ public class SearchUserFragment extends Fragment implements View.OnClickListener
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View root = inflater.inflate(R.layout.fragment_search_user, container, false);
-        layout = root.findViewById(R.id.fragment_search_layout);
+        layout = root.findViewById(R.id.companyLayout);
         userAvatar = root.findViewById(R.id.user_avatar);
         userNameText = root.findViewById(R.id.user_name);
         userLocationText = root.findViewById(R.id.user_location);
@@ -71,22 +82,46 @@ public class SearchUserFragment extends Fragment implements View.OnClickListener
 
     @Override
     public void onClick(View view) {
-        FragmentManager fragmentManager = getFragmentManager();
-        ReposFragment reposFragment = new ReposFragment();
-        Bundle bundle = new Bundle();
-        bundle.putString(KEY_COMPANY_NAME, companyName);
-        bundle.putString(KEY_NUMBER_REPO, numberRepo);
-        reposFragment.setArguments(bundle);
-        if (fragmentManager != null) {
-            fragmentManager.beginTransaction()
-                    .replace(R.id.fragmentContainer, reposFragment)
-                    .addToBackStack("userfragment")
-                    .commit();
+        userName = editText.getText().toString();
+        if(getContext() != null) {
+            if(isInternetConnected(getContext())) {
+                loadData();
+            }
         }
+
     }
 
     public void loadData() {
-        //TODO (3): Add data loading method.
+        progressBar.setVisibility(View.VISIBLE);
+        final GitHubUserCall apiService = GitHubApiClient.getClient().create(GitHubUserCall.class);
+        Call<GitHubUser> call = apiService.getUser(userName);
+        call.enqueue(new Callback<GitHubUser>() {
+            @Override
+            public void onResponse(@NonNull Call<GitHubUser> call, @NonNull Response<GitHubUser> response) {
+                if(response.body() != null) {
+                    progressBar.setVisibility(View.GONE);
+                    layout.setVisibility(View.VISIBLE);
+                    if (response.body().getUserName() != null) {
+                        userNameText.setText(response.body().getUserName());
+                    }
+                    userLocationText.setText(response.body().getUserLocation());
+                    userBlogText.setText(response.body().getUserBlog());
+                    Picasso.get()
+                            .load(response.body().getUserAvatar())
+                            .resize(150,150)
+                            .into(userAvatar);
+                    companyName = response.body().getUserName();
+                    numberRepo = response.body().getUserRepos();
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GitHubUser> call, Throwable t) {
+
+            }
+        });
 
     }
 }
