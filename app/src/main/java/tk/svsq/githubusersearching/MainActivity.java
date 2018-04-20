a4988fa0f13814aa0535eb4ab23d9bae4b71e3f9
Slaventij Bilokur <svsq@outlook.com>
2018-04-20 13:44:46 +0300

    Removed unused import

--- app/src/main/java/tk/svsq/githubusersearching/MainActivity.java
+++ app/src/main/java/tk/svsq/githubusersearching/MainActivity.java
@@ -3,7 +3,6 @@ package tk.svsq.githubusersearching;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import tk.svsq.githubusersearching.fragment.SearchUserFragment;
 
